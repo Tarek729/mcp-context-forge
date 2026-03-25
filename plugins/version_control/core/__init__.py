9c83e5ad2c0f7f084da73e9f2f8ecdbf6c70834a
Siddhesh Bangar <siddhesh@MacBookPro.fritz.box>
2026-03-25 22:02:55 +0000

    chore: remove unwanted files from PR

--- plugins/version_control/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core version control logic"""
-
-from .version_control_core import VersionControlCore, VersionControlDB, HashComputer
-
-__all__ = ["VersionControlCore", "VersionControlDB", "HashComputer"]
-
-# Made with Bob
